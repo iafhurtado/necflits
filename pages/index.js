20e26386490009f6a6dec42ae94694abba61576e
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-23 16:17:01 +0100

    YOUTube API in progress

--- pages/index.js
+++ pages/index.js
@@ -3,36 +3,49 @@ import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/banner";
 import NavBar from "../components/nav/navbar";
 import Card from "../components/card/card";
-import SectionCards from "@/components/card/section-cards";
-import { getVideos } from "@/lib/videos";
+import SectionCards from "../components/card/section-cards";
+import { getVideos } from "../lib/videos";
 
+export async function getServerSideProps(context) {
+  const disneyVideos = await getVideos("disney trailer");
+  const productivityVideos = await getVideos("Productivity");
 
-export default function Home() {
-  const disneyVideos = getVideos();
+  const travelVideos = await getVideos("indie music");
+  // const popularVideos = await getVideos();
+
+  return {
+    props: { disneyVideos, travelVideos, productivityVideos },
+  };
+}
+
+export default function Home({
+  disneyVideos,
+  travelVideos,
+  productivityVideos,
+}) {
+  console.log({ disneyVideos });
   return (
     <div className={styles.container}>
       <Head>
-        <title>Necflits</title>
+        <title>Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-
-      <NavBar username="John Doe" />
-
+      <NavBar username="ankita@ank.com" />
       <Banner
         title="Clifford the red dog"
         subTitle="a very cute dog"
         imgUrl="/static/clifford.webp"
       />
-
       <div className={styles.sectionWrapper}>
         <SectionCards title="Disney" videos={disneyVideos} size="large" />
-        <SectionCards title="Disney" videos={disneyVideos} size="medium" />
+        <SectionCards title="Travel" videos={travelVideos} size="small" />
+        <SectionCards
+          title="Productivity"
+          videos={productivityVideos}
+          size="medium"
+        />
+        <SectionCards title="Popular" videos={disneyVideos} size="small" />
       </div>
-      
-
-      {/* <Navbar />
-      <Card /> */}
     </div>
   );
 }
