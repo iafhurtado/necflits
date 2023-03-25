1261cbeed52c3e6c6e03052350df1e7f49f2ade0
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-25 12:22:30 +0100

    dynamic route for videos enhanced

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,9 +1,33 @@
 import { useRouter } from "next/router";
+import Modal from "react-modal";
+import styles from "../../styles/Video.module.css";
+
+Modal.setAppElement("#__next");
 
 const Video = () => {
     const router = useRouter();
   console.log({ router });
-  return <div>video page {router.query.videoId}</div>;
+  return (
+    <div className={styles.container}>
+      <Modal
+        isOpen={true}
+        contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
+        className={styles.modal}
+        overlayClassName={styles.overlay}
+      >
+      <iframe
+          id="ytplayer"
+          type="text/html"
+          width="100%"
+          height="360"
+          src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+          frameBorder="0"
+        ></iframe>
+        <div> Modal body sucka</div>
+      </Modal>
+    </div>
+  );
   };
   
   export default Video;
