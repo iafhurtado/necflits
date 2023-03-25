17d2532f54fc7748901ade948731501c4a23b1a4
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-25 22:37:39 +0100

    Incremental Site Regeneration with getStaticProps

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,12 +1,43 @@
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
+import clsx from "classnames";
 
 Modal.setAppElement("#__next");
+
+export async function getStaticProps() {
+  //data to fetch from API
+  return {
+    props: {
+      video,
+    },
+    revalidate: 10, // In seconds
+  };
+}
+
+export async function getStaticPaths() {
+  const listOfVideos = ["mYfJxlgR2jw", "4zH5iYM4wJo", "KCPEHsAViiQ"];
+  const paths = listOfVideos.map((videoId) => ({
+    params: { videoId },
+  }));
+
+  return { paths, fallback: "blocking" };
+}
+
 
 const Video = () => {
     const router = useRouter();
   console.log({ router });
+  const video = {
+    title: "Hi cute dog",
+    publishTime: "1990-01-01",
+    description:
+    "A big red dog that is super cute, can he get any bigger? A big red dog that is super cute, can he get any bigger?",
+    channelTitle: "Paramount Pictures",
+    viewCount: 10000,
+  };
+
+  const { title, publishTime, description, channelTitle, viewCount } = video;
   return (
     <div className={styles.container}>
       <Modal
@@ -18,12 +49,32 @@ const Video = () => {
       >
       <iframe
           id="ytplayer"
+          className={styles.videoPlayer}
           type="text/html"
           width="100%"
           height="360"
           src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
           frameBorder="0"
         ></iframe>
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
+            </div>
+          </div>
+        </div>
         <div> Modal body sucka</div>
       </Modal>
     </div>
