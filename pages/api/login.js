bbfdd2ffe793e8db601520e1277fa3743e64c28d
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-03-27 12:07:54 +0200

    added jwt and cookie

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,6 @@
 import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
+import { createNewUser, isNewUser } from "../../lib/db/hasura";
 
 
 export default async function login(req, res) {
@@ -6,12 +8,37 @@ export default async function login(req, res) {
       try {
         const auth = req.headers.authorization;
         const didToken = auth ? auth.substr(7) : "";
-        console.log({ didToken });
+        
+        // invoke magic
 
         const metadata = await magicAdmin.users.getMetadataByToken(didToken);
         console.log({ metadata });
-        //invoke magic
-        res.send({ done: true });
+        // create jwt
+
+        const token = jwt.sign(
+            {
+            ...metadata,
+            iat: Math.floor(Date.now() / 1000),
+            exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+            "https://hasura.io/jwt/claims": {
+                "x-hasura-allowed-roles": ["user", "admin"],
+                "x-hasura-default-role": "user",
+                "x-hasura-user-id": `${metadata.issuer}`,
+            },
+            },
+            "thisisasecretthisisasecrett1234"
+        );
+        console.log({ token });
+        // CHECK IF USER EXISTS
+
+        if (isNewUserQuery) {
+            const createNewUserMutation = await createNewUser(token, metadata);
+            console.log({ createNewUserMutation });
+            res.send({ done: true, msg: "is a new user" });
+          } else {
+            res.send({ done: true, msg: "not a new user" });
+          }
+        res.send({ done: true, isNewUserQuery });
       } catch (error) {
         console.error("Something went wrong logging in", error);
         res.status(500).send({ done: false });
